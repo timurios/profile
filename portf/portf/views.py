fb30eaf368ba2f763ae49baacee03f94dcfe126e
timurios <abdilovtimyr@gmail.com>
2021-10-10 22:37:01 +0600

    +about page

--- portf/portf/views.py
+++ portf/portf/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect, HttpResponse
 def main(request):
     return render(request, "tmp/index.html")
 
-
+def about(request):
+    return render(request, "tmp/about.html")
 
 
 
