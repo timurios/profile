c624a28677a745711556db526805c8da39f8be71
timurios <abdilovtimyr@gmail.com>
2021-10-10 15:01:37 +0600

    portf

---
+++ portf/portf/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect, HttpResponse
+
+def main(request):
+    return render(request, "tmp/index.html")
+
+
+
+
+
+ 
+
+
+
+
+
+
+
---
+++ portf/spacey/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpaceyConfig(AppConfig):
+    name = 'spacey'
